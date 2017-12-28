98910267fb155f2f3c73b4159d7471c26f833dc9
1000copy <1000copy@gmail.com>
2017-12-28 17:41:50 +0800

    add handle

--- codelabs/editor.js
+++ codelabs/editor.js
@@ -1,14 +1,137 @@
-class NewShapeBg{
+class Dragable{
+  constructor(element,offset){
+    self = this
+    this.element = element
+    this.offset = offset
+  }
+  initDrag(){
+     this.element.drag(
+        function(dx, dy, x, y,event){
+            //  dx = abs(startX - x) ,ditto dy .
+            self.drag(dx,dy,x - self.offset.left,y - self.offset.top)
+        },
+        function(x,y){
+            self.startDrag(x - self.offset.left,y - self.offset.top)
+        }
+        ,
+        function(){
+          self.stopDrag()
+        }
+      )
+  }
+  drag(dx,dy,x,y){}
+  stopDrag(){}
+  startDrag(x,y){}
+}
+class NewShapeBg extends Dragable{
    constructor(paper,offset, width, height){
-      self = this
+      var newShapeBackground = paper.rect(0, 0, width, height);
+      super(newShapeBackground,offset)
+      this.SelectState = true
+      this.newShapeBackground = newShapeBackground
       this.paper = paper
-      this.offset = offset
-      this.newShapeBackground = this.paper.rect(0, 0, width, height);
+      self = this
       this.newShapeBackground.attr({'fill':'#00a',
                             'fill-opacity':0.3,
                             'stroke-width': 1,
                             'cursor': 'default'});
-      this.newShapeBackground.drag(
+      // this.newShapeBackground.drag(
+      //   function(dx, dy, x, y,event){
+      //       //  dx = abs(startX - x) ,ditto dy .
+      //       self.drag(dx,dy,x - self.offset.left,y - self.offset.top)
+      //   },
+      //   function(x,y){
+      //       self.startDrag(x - self.offset.left,y - self.offset.top)
+      //   }
+      //   ,
+      //   function(){
+      //     self.stopDrag()
+      //   }
+      // )
+  }
+  drag(dx,dy,x,y){
+    if (!this.SelectState){
+      this.rect.setCoords(this.startX,this.startY,x,y)
+    }
+  }
+  stopDrag(){
+    if (!this.SelectState){
+      var coords = this.rect.getCoords();
+      if (coords.width < 2 || coords.height < 2) {
+        this.rect.destroy();
+        delete this.rect;
+        return;
+      }
+    }
+  }
+  startDrag(x,y){
+    if (!this.SelectState){
+    // if(true){
+      this.rect = new Rect({
+          'paper': this.paper,
+          'PP': this.PP,
+          'x': x,
+          'y': y,
+          'width': 0,
+          'height': 0});
+      this.newShapeBackground.toFront()
+      this.startX = x
+      this.startY = y
+    }
+  }
+}
+class Editor{
+  constructor(paper){
+    var elementId = paper.elementId, width=paper.width, height = paper.height
+    self = this
+    this.paper = paper.paper
+    
+    this.PP = paper
+    console.log(this.PP)
+    // this.$el = $("#" + elementId);
+    this.offset = paper.offset
+    console.log(this.offset)
+    this.newShapeBg = new NewShapeBg(this.paper,this.offset, width, height)
+    this.rect = new Rect({
+          'paper': this.paper,
+          'PP' : this.PP,
+          'offset': this.offset,
+          'x': 100,
+          'y': 100,
+          'width': 100,
+          'height': 100});
+  }
+  
+} 
+class Handle{
+  constructor(PP,owner){
+    this.handleSize = 8
+    this.owner = owner
+    this.PP = pp
+  }
+}
+class Rect{
+  constructor(options){
+    this.handleSize = 8
+    var self = this;
+    this.paper = options.paper;
+    this.PP = options.PP;
+    console.log(this.PP)
+    this.offset = options.offset
+    this._x = options.x;
+    this._y = options.y;
+    this._width = options.width;
+    this._height = options.height;
+    this._strokeColor = options.strokeColor || "red"
+    this._strokeWidth = options.strokeWidth || 2;
+    this._selected = false;
+
+    this.element = this.paper.rect();
+    this.element.attr({'fill-opacity': 0.01,
+                       'fill': '#fff',
+                       'cursor': 'pointer'});
+    this.drawShape();
+    this.element.drag(
         function(dx, dy, x, y,event){
             //  dx = abs(startX - x) ,ditto dy .
             self.drag(dx,dy,x - self.offset.left,y - self.offset.top)
@@ -23,57 +146,18 @@ class NewShapeBg{
       )
   }
   drag(dx,dy,x,y){
-    this.rect.setCoords(this.startX,this.startY,x,y)
+
   }
   stopDrag(){
-    var coords = this.rect.getCoords();
-    if (coords.width < 2 || coords.height < 2) {
-      this.rect.destroy();
-      delete this.rect;
-      return;
-    }
+
   }
   startDrag(x,y){
-    this.rect = new Rect({
-        'paper': this.paper,
-        'x': x,
-        'y': y,
-        'width': 0,
-        'height': 0});
-    this.newShapeBackground.toFront()
-    this.startX = x
-    this.startY = y
-  }
-}
-
-class Editor{
-  constructor(Raphael,elementId, width, height){
-    self = this
-    this.paper = Raphael(elementId, width, height);
-    this.$el = $("#" + elementId);
-    this.offset = self.$el.offset(),
-    this.newShapeBg = new NewShapeBg(this.paper,this.offset, width, height)
-  }
-  
-} 
-
-class Rect{
-  constructor(options){
-    var self = this;
-    this.paper = options.paper;
-    
-    this._x = options.x;
-    this._y = options.y;
-    this._width = options.width;
-    this._height = options.height;
-    this._strokeColor = options.strokeColor || "red"
-    this._strokeWidth = options.strokeWidth || 2;
-    this._selected = false;
-    this.element = this.paper.rect();
-    this.element.attr({'fill-opacity': 0.01,
-                       'fill': '#fff',
-                       'cursor': 'pointer'});
-    this.drawShape();
+    this.isSelected = !this.isSelected
+    if(!this.handle){
+      console.log(this.PP)
+      this.handle = this.PP.rectByCenter(this._x, this._y, this.handleSize, this.handleSize);
+    }
+    this.drawShape()
   }
   drawShape() {
     var x = this._x ,
@@ -84,6 +168,13 @@ class Rect{
                        'width':w, 'height':h,
                        'stroke': this._strokeColor,
                        'stroke-width': this._strokeWidth});
+    console.log(this.isSelected)
+    if (this.handle)
+      if(this.isSelected){
+         this.handle.show()
+      }else{
+         this.handle.hide()
+      }
   }
   setCoords(x1,y1,x2,y2) {
     var coords = {'x': Math.min(x1, x2),
