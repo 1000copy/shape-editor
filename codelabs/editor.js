410bc5395c0d828c3ae6a52f91901bf4394a1d46
1000copy <1000copy@gmail.com>
2018-01-02 11:56:37 +0800

    select add

--- codelabs/editor.js
+++ codelabs/editor.js
@@ -1,10 +1,10 @@
 class Dragable{
   constructor(element,offset){
-    self = this
     this.element = element
     this.offset = offset
+    // this.initDrag(this)
   }
-  initDrag(){
+  initDrag(self){
      this.element.drag(
         function(dx, dy, x, y,event){
             //  dx = abs(startX - x) ,ditto dy .
@@ -24,38 +24,32 @@ class Dragable{
   startDrag(x,y){}
 }
 class NewShapeBg extends Dragable{
-   constructor(paper,offset, width, height){
-      var newShapeBackground = paper.rect(0, 0, width, height);
+   constructor(editor,offset, width, height){
+      var newShapeBackground = editor.makeRect(0, 0, width, height);
       super(newShapeBackground,offset)
+
+      this.editor = editor
+      this.offset = offset
       this.SelectState = true
       this.newShapeBackground = newShapeBackground
-      this.paper = paper
       self = this
       this.newShapeBackground.attr({'fill':'#00a',
                             'fill-opacity':0.3,
                             'stroke-width': 1,
                             'cursor': 'default'});
-      // this.newShapeBackground.drag(
-      //   function(dx, dy, x, y,event){
-      //       //  dx = abs(startX - x) ,ditto dy .
-      //       self.drag(dx,dy,x - self.offset.left,y - self.offset.top)
-      //   },
-      //   function(x,y){
-      //       self.startDrag(x - self.offset.left,y - self.offset.top)
-      //   }
-      //   ,
-      //   function(){
-      //     self.stopDrag()
-      //   }
-      // )
+      this.newShapeBackground.toFront()
+      this.initDrag(this)
+  }
+  toBack(){
+    this.element.toBack()
   }
   drag(dx,dy,x,y){
-    if (!this.SelectState){
+    if (!this.editor.isSelectedState()){
       this.rect.setCoords(this.startX,this.startY,x,y)
     }
   }
   stopDrag(){
-    if (!this.SelectState){
+    if (!this.editor.isSelectedState()){
       var coords = this.rect.getCoords();
       if (coords.width < 2 || coords.height < 2) {
         this.rect.destroy();
@@ -65,15 +59,8 @@ class NewShapeBg extends Dragable{
     }
   }
   startDrag(x,y){
-    if (!this.SelectState){
-    // if(true){
-      this.rect = new Rect({
-          'paper': this.paper,
-          'PP': this.PP,
-          'x': x,
-          'y': y,
-          'width': 0,
-          'height': 0});
+    if (!this.editor.isSelectedState()){
+      this.rect = new Rect(this.editor,x,y,0,0);
       this.newShapeBackground.toFront()
       this.startX = x
       this.startY = y
@@ -86,64 +73,50 @@ class Editor{
     self = this
     this.paper = paper.paper
     
-    this.PP = paper
-    console.log(this.PP)
-    // this.$el = $("#" + elementId);
-    this.offset = paper.offset
-    console.log(this.offset)
-    this.newShapeBg = new NewShapeBg(this.paper,this.offset, width, height)
-    this.rect = new Rect({
-          'paper': this.paper,
-          'PP' : this.PP,
-          'offset': this.offset,
-          'x': 100,
-          'y': 100,
-          'width': 100,
-          'height': 100});
-  }
-  
-} 
-class Handle{
-  constructor(PP,owner){
-    this.handleSize = 8
-    this.owner = owner
-    this.PP = pp
-  }
-}
-class Rect{
-  constructor(options){
-    this.handleSize = 8
-    var self = this;
-    this.paper = options.paper;
-    this.PP = options.PP;
-    console.log(this.PP)
-    this.offset = options.offset
-    this._x = options.x;
-    this._y = options.y;
-    this._width = options.width;
-    this._height = options.height;
-    this._strokeColor = options.strokeColor || "red"
-    this._strokeWidth = options.strokeWidth || 2;
-    this._selected = false;
 
-    this.element = this.paper.rect();
+    this.offset = paper.offset
+    this.newShapeBg = new NewShapeBg(this,this.offset, width, height)
+    this.rect = new Rect(this,100,100,100,100)
+    this.setState("select")
+  }
+  setState(state){
+    this.state = state
+    if (this.isSelectedState()){
+      this.newShapeBg.toBack()
+    }
+  }
+  isSelectedState(){
+    return this.state === "select"
+  }
+  makeRect(x,y,width,height){
+    return this.paper.rect(x,y,width,height)
+  }
+  rectByCenter(x,y,width,height){
+      return this.paper.rect(x - width/2,y - height/2,width,height)
+  }  
+} 
+class Rect extends Dragable{
+  constructor(editor,x,y,width,height){
+    var element = editor.makeRect();
+    super(element,editor.offset)
+    this.handleSize = 8
+    this.editor = editor
+    this.offset = editor.offset
+    this._x = x;
+    this._y = y;
+    this._width = width;
+    this._height = height;
+    this._strokeColor =  "red"
+    this._strokeWidth =  2;
+    this._selected = false;
+    this.element = element
     this.element.attr({'fill-opacity': 0.01,
                        'fill': '#fff',
                        'cursor': 'pointer'});
-    this.drawShape();
-    this.element.drag(
-        function(dx, dy, x, y,event){
-            //  dx = abs(startX - x) ,ditto dy .
-            self.drag(dx,dy,x - self.offset.left,y - self.offset.top)
-        },
-        function(x,y){
-            self.startDrag(x - self.offset.left,y - self.offset.top)
-        }
-        ,
-        function(){
-          self.stopDrag()
-        }
-      )
+    this.handles = new Handles(this.editor)
+    this.drawShape()
+    var self = this
+    this.initDrag(this)
   }
   drag(dx,dy,x,y){
 
@@ -151,14 +124,13 @@ class Rect{
   stopDrag(){
 
   }
+
   startDrag(x,y){
     this.isSelected = !this.isSelected
-    if(!this.handle){
-      console.log(this.PP)
-      this.handle = this.PP.rectByCenter(this._x, this._y, this.handleSize, this.handleSize);
-    }
+    this.handles.drawHandles(this._x,this._y,this._width,this._height)
     this.drawShape()
   }
+  
   drawShape() {
     var x = this._x ,
         y = this._y ,
@@ -168,13 +140,7 @@ class Rect{
                        'width':w, 'height':h,
                        'stroke': this._strokeColor,
                        'stroke-width': this._strokeWidth});
-    console.log(this.isSelected)
-    if (this.handle)
-      if(this.isSelected){
-         this.handle.show()
-      }else{
-         this.handle.hide()
-      }
+    this.handles.show(this.isSelected)
   }
   setCoords(x1,y1,x2,y2) {
     var coords = {'x': Math.min(x1, x2),
@@ -198,4 +164,44 @@ class Rect{
   destroy() {
     this.element.remove();
   };
+}
+class Handles{
+  constructor(editor){
+    this.editor = editor
+    this.handles = []
+    this.handleSize = 8
+  }
+  drawHandles(x,y,width,height){
+    var resultxys = this.HandlesCords (x,y,width,height)
+    for (var i = 0; i < resultxys.length; i++) {
+      var x = resultxys[i][0]
+      var y = resultxys[i][1]
+      this.handles.push(this.makeHandle(x,y)) 
+    }
+  }
+  makeHandle(x,y){
+    return this.editor.rectByCenter(x, y, this.handleSize, this.handleSize);
+  }
+  HandlesCords(x,y,width,height){
+    var xs = [x,x + width/2,x+width]
+    var ys = [y,y + height/2,y+height]
+    var resultxys = []
+    for (var i = 0; i < xs.length; i++) {
+      for (var j = 0; j < ys.length; j++) {
+        if (i != 1 || j != 1)
+          resultxys.push([xs[i],ys[j]])
+      }
+    }
+    return resultxys
+  }
+  show(isSelected){
+     for (var i = 0; i < this.handles.length; i++) {
+        var handle = this.handles[i]
+         if(isSelected){
+           handle.show()
+        }else{
+           handle.hide()
+        }
+      }
+  }
 }
